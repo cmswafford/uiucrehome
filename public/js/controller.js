56032a78f3bd59eae64d9de87a07fcd4386036fe
Clifton Swafford <clifton@varlabs.com>
2011-04-24 16:53:20 -0500

    Updated controller for all the extra pages.

--- public/js/controller.js
+++ public/js/controller.js
@@ -7,10 +7,10 @@
 /**
  * Prevent default horizontal scrolling behavior of the ipad  
  */
-/*document.body.addEventListener('touchmove', function(e) {
+document.body.addEventListener('touchmove', function(e) {
 	  // This prevents native scrolling from happening.
 	  e.preventDefault();
-}, false);*/
+}, false);
 
 // Portal Page link handler
 var bindNavigationButtons = function() {
@@ -28,8 +28,91 @@ var bindNavigationButtons = function() {
 		});
 	});
 };
-
 bindNavigationButtons();
+
+var bindHvacButtons = function() {
+	$(".thermoButton").bind("mousedown", function(e) {
+		var id = this.id;	
+		if (id == 'z1u') {
+			setting = parseFloat($('#z1Temp').html());
+			if(setting < 90) setting += 1;
+			$('#z1Temp').html(setting.toString());
+			// send message
+		}
+		else if (id == 'z2u') {
+			setting = parseFloat($('#z2Temp').html());
+			if(setting < 90) setting += 1;
+			$('#z2Temp').html(setting.toString());
+			// send message
+		}
+		else if (id == 'z1d') {
+			setting = parseFloat($('#z1Temp').html());
+			if(setting > 40) setting -= 1;
+			$('#z1Temp').html(setting.toString());
+			// send message
+		}
+		else {
+			setting = parseFloat($('#z2Temp').html());
+			if(setting > 40) setting -= 1;
+			$('#z2Temp').html(setting.toString());
+			// send message
+		}
+	});
+};
+bindHvacButtons();
+
+var bindCategoryButtons = function() {
+	$(".category").bind("click", function(e) {
+		var requestedID = this.id;
+		$('.device').removeClass('deviceSelected');
+		if (requestedID == 'homeCategory') {
+			$('#space').html("<h3>Entire House</h3>");
+			$('.device').addClass('deviceSelected');
+			Cufon.replace('h3', {fontFamily: 'Thin'});
+			//etc
+		}
+		else if (requestedID == 'lightingCategory') {
+			$('#space').html("<h3>All Lights</h3>");
+			$('.light').addClass('deviceSelected');
+			Cufon.replace('h3', {fontFamily: 'Thin'});
+			//etc
+		}
+		else if (requestedID == 'appliancesCategory') {
+			$('#space').html("<h3>All Appliances</h3>");
+			$('.appliance').addClass('deviceSelected');
+			Cufon.replace('h3', {fontFamily: 'Thin'});
+			//etc
+		}
+	});
+};
+bindCategoryButtons();
+
+var bindDeviceButtons = function() {
+	$(".device").bind("click", function(e) {
+		$('#space').html("<h3>"+this.id+"</h3>");
+		$('.device').removeClass('deviceSelected');
+		Cufon.replace('h3', {fontFamily: 'Thin'});
+		$('#'+this.id).addClass('deviceSelected');
+		//etc
+	});
+};
+bindDeviceButtons();
+
+var bindHeatSwitch = function() {
+	$(".hSwitch").bind("click", function(e) {
+		if($('#waterSwitchHorizontal').hasClass('offSwitch')){
+			$("#waterSwitchHorizontal").stop().animate({'bottom': '105px', 'background': '-webkit-gradient(linear, left top, left bottom, from(#ff3750), color-stop(0.40,rgba(233,20,20,1)), color-stop(0.45,rgba(200,20,20,1)), to(#5f000c))'}, 80, function() {
+				$('#waterSwitchHorizontal').removeClass("offSwitch"); 
+			});
+		}
+		else {
+			$("#waterSwitchHorizontal").stop().animate({'bottom': '45px', 'background': '-webkit-gradient(linear, left top, left bottom, from(#999), color-stop(0.40,rgba(153,153,153,1)), color-stop(0.45,rgba(130,130,130,1)), to(#333))'}, 80, function() {
+				$('#waterSwitchHorizontal').addClass("offSwitch"); 
+			});
+		}
+	});
+};
+bindHeatSwitch();
 
 var bindGraphButtons = function() {
 	$(".arrow").bind("click", handler = function(e) {
@@ -52,7 +135,7 @@ var bindGraphButtons = function() {
 			else {
 				leftMargin += 800;
 				setIndicator(leftMargin);
-				$("#centerGraphLandscape").stop().animate({'margin-left': leftMargin}, 300);
+				$("#centerGraphLandscape").stop().css('margin-left', leftMargin);
 			}
 		}
 		else {
@@ -60,12 +143,11 @@ var bindGraphButtons = function() {
 			else {
 				leftMargin -= 800;
 				setIndicator(leftMargin);
-				$("#centerGraphLandscape").stop().animate({'margin-left': leftMargin}, 300);
+				$("#centerGraphLandscape").stop().css('margin-left', leftMargin);
 			}
 		}
 	});
 };
-
 bindGraphButtons();
 
 var bindThermGraphButtons = function() {
@@ -89,7 +171,7 @@ var bindThermGraphButtons = function() {
 			else {
 				leftMargin += 800;
 				setIndicator(leftMargin);
-				$("#thermCenterGraphLandscape").stop().animate({'margin-left': leftMargin}, 300);
+				$("#thermCenterGraphLandscape").stop().css('margin-left', leftMargin);
 			}
 		}
 		else {
@@ -97,13 +179,84 @@ var bindThermGraphButtons = function() {
 			else {
 				leftMargin -= 800;
 				setIndicator(leftMargin);
-				$("#thermCenterGraphLandscape").stop().animate({'margin-left': leftMargin}, 300);
+				$("#thermCenterGraphLandscape").stop().css('margin-left', leftMargin);
 			}
 		}
 	});
 };
-
 bindThermGraphButtons();
+
+var bindSunGraphButtons = function() {
+	$(".sunArrow").bind("click", handler = function(e) {
+		var leftMargin = $("#sunCenterGraphLandscape").css("margin-left");
+		leftMargin = leftMargin.substring(0, leftMargin.length-2);
+		leftMargin = parseInt(leftMargin);
+		leftMargin = leftMargin - leftMargin%800;
+		console.log("landscape element has a margin of " + leftMargin);
+		var setIndicator = function(leftMargin) {
+			var indicator;
+			if(leftMargin == 0) indicator = "sunIndicator1";
+			if(leftMargin == -800) indicator = "sunIndicator2";
+			if(leftMargin == -1600) indicator = "sunIndicator3";
+			if(leftMargin == -2400) indicator = "sunIndicator4";
+			$('.sunIndicator').removeClass('indicating');
+			$('#'+indicator).addClass('indicating');
+		}
+		if(this.id == "sunLeftArrow") {
+			if(leftMargin >= 0) return;
+			else {
+				leftMargin += 800;
+				setIndicator(leftMargin);
+				$("#sunCenterGraphLandscape").stop().css('margin-left', leftMargin);
+			}
+		}
+		else {
+			if(leftMargin <= -2400) return;
+			else {
+				leftMargin -= 800;
+				setIndicator(leftMargin);
+				$("#sunCenterGraphLandscape").stop().css('margin-left', leftMargin);
+			}
+		}
+	});
+};
+bindSunGraphButtons();
+
+var bindWaterGraphButtons = function() {
+	$(".waterArrow").bind("click", handler = function(e) {
+		var leftMargin = $("#waterCenterGraphLandscape").css("margin-left");
+		leftMargin = leftMargin.substring(0, leftMargin.length-2);
+		leftMargin = parseInt(leftMargin);
+		leftMargin = leftMargin - leftMargin%800;
+		console.log("landscape element has a margin of " + leftMargin);
+		var setIndicator = function(leftMargin) {
+			var indicator;
+			if(leftMargin == 0) indicator = "waterIndicator1";
+			if(leftMargin == -800) indicator = "waterIndicator2";
+			if(leftMargin == -1600) indicator = "waterIndicator3";
+			if(leftMargin == -2400) indicator = "waterIndicator4";
+			$('.waterIndicator').removeClass('indicating');
+			$('#'+indicator).addClass('indicating');
+		}
+		if(this.id == "waterLeftArrow") {
+			if(leftMargin >= 0) return;
+			else {
+				leftMargin += 800;
+				setIndicator(leftMargin);
+				$("#waterCenterGraphLandscape").stop().css('margin-left', leftMargin);
+			}
+		}
+		else {
+			if(leftMargin <= -2400) return;
+			else {
+				leftMargin -= 800;
+				setIndicator(leftMargin);
+				$("#waterCenterGraphLandscape").stop().css('margin-left', leftMargin);
+			}
+		}
+	});
+};
+bindWaterGraphButtons();
 
 var bindHistoryGraphButtons = function() {
 	$(".monitorArrow").bind("click", handler = function(e) {
@@ -127,7 +280,7 @@ var bindHistoryGraphButtons = function() {
 			else {
 				leftMargin += 480;
 				setIndicator(leftMargin);
-				$("#historyCenterGraphLandscape").stop().animate({'margin-left': leftMargin}, 300);
+				$("#historyCenterGraphLandscape").stop().css('margin-left', leftMargin);
 			}
 		}
 		else {
@@ -135,19 +288,20 @@ var bindHistoryGraphButtons = function() {
 			else {
 				leftMargin -= 480;
 				setIndicator(leftMargin);
-				$("#historyCenterGraphLandscape").stop().animate({'margin-left': leftMargin}, 300);
+				$("#historyCenterGraphLandscape").stop().css('margin-left', leftMargin);
 			}
 		}
 	});
 };
-
 bindHistoryGraphButtons();
 
-// This not working, this div is obscured
-$("#wholeHouse").click(function (){
-	document.getElementById("space").innerHTML = "<h3>Entire House</h3>";
-	Cufon.replace('h3', { fontFamily: 'Thin' }); // Font Replacement: update the replacement at every refresh
-});
-
+var bindMonitorSelectorButtons = function() {
+	$(".glossy").bind("click", handler = function(e) {
+		$('.glossy').removeClass('selected');
+		$('#'+this.id).addClass('selected');
+		//do something else
+	});
+};
+bindMonitorSelectorButtons();
 
 // perhaps a touch gesture listener which allows for pageflipping through the subpages
